3c5901f7b8deeeeb6aa241ac2eac85e65f2703fc
Khasbulat <khas.2015@gmail.com>
2022-07-27 04:22:09 +0500

    внедрение зависимостей

--- cmd/main.go
+++ cmd/main.go
@@ -6,10 +6,14 @@ import (
 	todo "github.com/Yakwilik/GoRestApiServiceToDo"
 
 	handler "github.com/Yakwilik/GoRestApiServiceToDo/pkg/handler"
+	"github.com/Yakwilik/GoRestApiServiceToDo/pkg/repository"
+	"github.com/Yakwilik/GoRestApiServiceToDo/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
